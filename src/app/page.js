228480647345d4f7678f78485c042e539041f084
dinda <kitadinda66@gmail.com>
2024-07-01 15:15:44 +0700

    Add large folder 'my-project' using Git LFS

---
+++ src/app/page.js
@@ -0,0 +1,53 @@
+"use client";  // Tambahkan ini di bagian atas file
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Login() {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Validasi login di sini
+        // Contoh sederhana, arahkan ke halaman home jika berhasil
+        router.push('/home');
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md bg-white p-8 border border-gray-300 rounded-lg">
+                <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Email:</label>
+                        <input 
+                            type="email" 
+                            value={email} 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            required 
+                            className="w-full p-2 border border-gray-300 rounded mt-1 text-black"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label className="block text-gray-700">Password:</label>
+                        <input 
+                            type="password" 
+                            value={password} 
+                            onChange={(e) => setPassword(e.target.value)} 
+                            required 
+                            className="w-full p-2 border border-gray-300 rounded mt-1 text-black"
+                        />
+                    </div>
+                    <button 
+                        type="submit" 
+                        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+                    >
+                        LOGIN
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
