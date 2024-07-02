2f46a3cbcd9c62f469b1dd2e0377ff76f338db34
dinda <kitadinda66@gmail.com>
2024-07-02 21:48:21 +0700

    Add large files using Git LFS

--- src/app/page.js
+++ src/app/page.js
@@ -1,53 +1,34 @@
-"use client";  // Tambahkan ini di bagian atas file
+import React from 'react';
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-
-export default function Login() {
-    const router = useRouter();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Validasi login di sini
-        // Contoh sederhana, arahkan ke halaman home jika berhasil
-        router.push('/home');
-    };
-
+export default function SuperqurbanKambing() {
     return (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md bg-white p-8 border border-gray-300 rounded-lg">
-                <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Email:</label>
-                        <input 
-                            type="email" 
-                            value={email} 
-                            onChange={(e) => setEmail(e.target.value)} 
-                            required 
-                            className="w-full p-2 border border-gray-300 rounded mt-1 text-black"
-                        />
+        <div className="bg-gray-100 p-10 min-h-screen flex items-center justify-center">
+            <div className="container mx-auto p-8 bg-white border border-gray-300 rounded-lg">
+            <h1 className="text-2xl font-bold mb-2 text-black">Superqurban Kambing</h1>
+            <p className="text-lg text-gray-500 mb-4">Manfaat qurban Anda, jadi semakin luas</p>
+              
+            <div className="border border-gray-300 rounded-lg p-4" style={{ borderStyle: 'dashed', borderWidth: '2px' }}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                    <div className="flex items-center p-4 pl-10 border-r border-gray-300" style={{ borderRightStyle: 'dashed', borderRightWidth: '2px' }}>
+                        <img src="/images/1.png" alt="Icon 1" className="h-12 w-12 mr-4" />
+                        <div>
+                            <div className="text-xl font-bold mb-1 text-black">789.500</div>
+                            <div className="text-gray-400">Pequrban (2023)</div>
+                        </div>
                     </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Password:</label>
-                        <input 
-                            type="password" 
-                            value={password} 
-                            onChange={(e) => setPassword(e.target.value)} 
-                            required 
-                            className="w-full p-2 border border-gray-300 rounded mt-1 text-black"
-                        />
+                    <div className="flex items-center p-4 pl-10">
+                        <img src="/images/2.png" alt="Icon 2" className="h-12 w-12 mr-4" />
+                        <div>
+                            <div className="text-xl font-bold mb-1 text-black">98.213</div>
+                            <div className="text-gray-400">Penerima Manfaat (2023)</div>
+                        </div>
                     </div>
-                    <button 
-                        type="submit" 
-                        className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
-                    >
-                        LOGIN
-                    </button>
-                </form>
+                </div>
             </div>
-        </div>
+
+</div>
+
+            </div>
+        
     );
 }
